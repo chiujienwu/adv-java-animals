2f31ca511a719cd650e26da62d396cf66a0ca269
chiujienwu <jchiu1@my.wctc.edu>
2020-10-26 07:17:22 -0500

    completed menu class

--- src/us/mattgreen/Main.java
+++ src/us/mattgreen/Main.java
@@ -11,9 +11,11 @@ public class Main {
         ArrayList<Talkable> zoo = new ArrayList<>();
 
         // Lines to Replace Begin Here
-        zoo.add(new Dog(true, "Bean"));
-        zoo.add(new Cat(9, "Charlie"));
-        zoo.add(new Teacher(44, "Stacy Read"));
+//        zoo.add(new Dog(true, "Bean"));
+//        zoo.add(new Cat(9, "Charlie"));
+//        zoo.add(new Teacher(44, "Stacy Read"));
+        Menu menu = new Menu(zoo);
+
         // End Lines to Replace
 
         for (Talkable thing : zoo) {
--- src/us/mattgreen/Menu.java
+++ src/us/mattgreen/Menu.java
@@ -1,18 +1,58 @@
 package us.mattgreen;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Menu {
 
     Scanner scanner = new Scanner(System.in);
 
-    public Menu () {
+    public Menu(ArrayList<Talkable> list) {
         System.out.println("Menu");
         System.out.println("====");
         System.out.println("What animal do you wish to create:");
         System.out.println("(c)at or (d)og?");
-        String input = scanner.nextLine();
+        String input = scanner.nextLine().toLowerCase();
 
+        switch (input) {
+            case "c":
+                System.out.println("What is the cat's name?");
+                String catName = scanner.nextLine();
+                System.out.println("How many mouse has the cat killed?");
+                int mouse = scanner.nextInt();
 
+                try {
+                    Cat cat = new Cat(mouse, catName);
+                    list.add(cat);
+                } catch (Exception e) {
+                    System.out.println("Something went wrong.");
+                }
+
+                break;
+
+            case "d":
+                System.out.println("What is the dog's name?");
+                String dogName = scanner.nextLine();
+                System.out.println("Is the dog friendly?");
+                System.out.println("(y)es or (n)o?");
+                String friend = scanner.nextLine().toLowerCase();
+
+                boolean friendly = false;
+                if (friend.equals("y")) {
+                    friendly = true;
+                }
+
+                try {
+                    Dog dog = new Dog(friendly, dogName);
+                    list.add(dog);
+                } catch (Exception e) {
+                    System.out.println("Something went wrong.");
+                }
+
+                break;
+
+            default:
+                System.out.println("Invalid selection.");
+        }
     }
 }
